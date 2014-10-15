354fc78f92452deb607befdefc8317c9aa107f5b
tmogrady <christina.ogrady@gmail.com>
2014-10-15 16:49:45 -0500

    First try at makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,18 @@
-## Added this comment to play with Git
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) inv <<- solve
+        getinverse <- function() inv
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
